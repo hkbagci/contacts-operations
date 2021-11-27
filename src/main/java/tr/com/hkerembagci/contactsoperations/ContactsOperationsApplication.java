eda07db893683c61d6ea339c79033431739171d5
hkbagci <hkerembagci@gmail.com>
2021-11-27 22:56:36 +0300

    Initial commit

---
+++ src/main/java/tr/com/hkerembagci/contactsoperations/ContactsOperationsApplication.java
@@ -0,0 +1,13 @@
+package tr.com.hkerembagci.contactsoperations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContactsOperationsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ContactsOperationsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tr/com/hkerembagci/contactsoperations/ContactsOperationsApplicationTests.java
@@ -0,0 +1,13 @@
+package tr.com.hkerembagci.contactsoperations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContactsOperationsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
